7fc4022ca8a1073e3b49b4fd50a6548cafadeb26
OMI786I <sohrawardy1998@gmail.com>
2024-11-25 23:36:31 +0600

    added buttons

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { BackgroundBeams } from "./ui/background-beams";
 import { GlareImage } from "./GlareImage";
-
+import { Link } from "react-router-dom";
 export function Banner() {
   return (
     <div className="h-[40rem] w-full  rounded-md bg-neutral-950 relative flex flex-col   justify-center antialiased">
@@ -30,7 +30,80 @@ export function Banner() {
           <GlareImage />
         </div>
       </div>
-      <div className="z-10 flex justify-center"></div>
+      <div className="z-10 flex justify-center gap-5 mt-5">
+        <Link
+          to={
+            "https://drive.google.com/file/d/1mmdYFX4UJDsXowatXEVwXBc2uAAAmwXA/view?usp=drive_link"
+          }
+        >
+          <button className="px-8 py-2 rounded-md bg-[#5C8374] text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500 flex">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-download"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
+              <path d="M7 11l5 5l5 -5" />
+              <path d="M12 4l0 12" />
+            </svg>
+            Download Resume
+          </button>
+        </Link>
+        <Link
+          to={"https://www.linkedin.com/in/tahid-sohrawardy-omi-040b93332/"}
+        >
+          <button className="px-8 flex py-2 rounded-md bg-[#5C8374] text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-brand-linkedin"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8 11v5" />
+              <path d="M8 8v.01" />
+              <path d="M12 16v-5" />
+              <path d="M16 16v-3a2 2 0 1 0 -4 0" />
+              <path d="M3 7a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v10a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4z" />
+            </svg>
+            Linkedin
+          </button>
+        </Link>
+        <Link to={"https://github.com/OMI786I"}>
+          <button className="px-8 flex py-2 rounded-md bg-[#5C8374] text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-brand-github"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
+            </svg>
+            Github
+          </button>
+        </Link>
+      </div>
       <BackgroundBeams className="" />
     </div>
   );
