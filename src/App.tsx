57bddd00e60288944ec51cb2f3944cef1278c19c
OMI786I <sohrawardy1998@gmail.com>
2024-11-25 15:18:39 +0600

    added hire me button

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,20 @@ import { NavbarDemo } from "./components/NavbarDemo";
 function App() {
   return (
     <div className="bg-[#040D12] min-h-screen">
-      <NavbarDemo />
+      {/**navbar */}
+      <div className="flex flex-row-reverse ">
+        <div>
+          {" "}
+          <NavbarDemo />
+        </div>
+
+        <button className="p-[3px] relative mt-5">
+          <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+          <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
+            Hire Me!
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
