711ba0511266ae6db1522ceeb922f23b4f352fd3
OMI786I <sohrawardy1998@gmail.com>
2024-11-27 14:25:16 +0600

    updated for deployment

--- src/components/GlareImage.tsx
+++ src/components/GlareImage.tsx
@@ -3,7 +3,7 @@ import { GlareCard } from "./ui/glare-card";
 export function GlareImage() {
   return (
     <GlareCard className="flex flex-col items-center justify-center">
-      <img src="../../src/assets/IMG-20241107-WA0070.jpg" />
+      <img src="/src/assets/IMG-20241107-WA0070.jpg" />
     </GlareCard>
   );
 }
