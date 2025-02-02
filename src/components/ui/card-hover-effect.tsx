e2f9bf3d1488e369fcc5c09602a36f21878aa38a
OMI786I <sohrawardy1998@gmail.com>
2025-02-03 02:18:07 +0600

    added kenakata project

--- src/components/ui/card-hover-effect.tsx
+++ src/components/ui/card-hover-effect.tsx
@@ -21,7 +21,7 @@ export const HoverEffect = ({
   return (
     <div
       className={cn(
-        "grid grid-cols-1 md:grid-cols-3  w-full   py-10",
+        "grid grid-cols-1 md:grid-cols-2  w-full   py-10",
         className
       )}
     >
